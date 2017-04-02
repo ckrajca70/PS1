2011b57e36f25999131a35d916d9668dbc5594c6
ckrajca70 <ckrajca_70@att.net>
2017-04-01 23:50:32 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS_DelegatesAndEvents
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+
+            del1(5, WorkType.Golf);
+            del2(10, WorkType.GenerateReports);
+
+            Console.Read();
+        }
+
+        static void WorkPerformed1(int hours,WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed1 called " + hours.ToString());
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed2 called " + hours.ToString());
+        }
+
+    }
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS_DelegatesAndEvents
+{
+    //public delegate int WorkPerformedHandler(int hours, WorkType workType);
+    public class worker
+    {
+        //public event
+
+    }
+}
