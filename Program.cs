dd0f83719eb60e2fe2cefc04b40a8398eb2a5d77
ckrajca70 <ckrajca_70@att.net>
2017-04-03 20:50:30 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,38 +6,137 @@ using System.Threading.Tasks;
 
 namespace PS_DelegatesAndEvents
 {
-    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    //public delegate int WorkPerformedHandler(int hours, WorkType workType);
+
+    //new custom delegate for Lambda
+    public delegate int BizRulesDelegate(int x, int y);
 
     class Program
     {
         static void Main(string[] args)
         {
-            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
-            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            var custs = new List<Customer>
+            {
+                new Customer{City = "Phoenix",FirstName="John",LastName = "Doe",ID=1 },
+                new Customer{City = "Phoenix",FirstName="Jane",LastName = "Doe",ID=500 },
+                new Customer{City = "Seattle",FirstName="Suki",LastName = "Pizzoro",ID=3 },
+                new Customer{City = "New York City",FirstName="Michelle",LastName = "Smith",ID=4 }
+            };
+
+            var phxCusts = custs
+                .Where(c => c.City == "Phoenix" && c.ID < 500)
+                .OrderBy(c => c.FirstName);
+
+            foreach(var cust in phxCusts)
+            {
+                Console.WriteLine(cust.City);
+            }
+
+            var data = new ProcessData();
+            BizRulesDelegate addDel = (x, y) => x + y;
+            BizRulesDelegate multiplyDel = (x, y) => x * y;
+            data.Process(2, 3,addDel);
+
+            //Func<t,TResult> example
+            Func<int, int, int> funcAddDel = (x, y) => x + y;
+            Func<int, int, int> funcMultiplyDel = (x, y) => x * y;
+            data.ProcessFunc(3, 2, funcAddDel);
+
+            //data.Process(2, 3, multiplyDel);
+            
+            //Action<T> example
+            Action<int, int> myAction = (x, y) => Console.WriteLine(x + y);
+            Action<int, int> myMultiplyAction = (x, y) => Console.WriteLine(x * y);
+            data.ProcessAction(2, 3, myAction);
+
+            //WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
 
 
             //del1(5, WorkType.Golf);
             //del2(10, WorkType.GenerateReports);
 
-            DoWork(del1);
+            //DoWork(del1);
+
+            ////del1 += del2;
+            ////del1 += del3;
+
+            //del1 += del2 + del3;
+
+            //int finalHours = del1(10, WorkType.GenerateReports);
+
+            //Console.WriteLine(finalHours);
+
+            var worker = new worker();
+            //worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
+            //worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
+
+            //modified to use Delegate Inference
+            //worker.WorkPerformed += Worker_WorkPerformed;
+
+            //////mod to use Anonymous 
+            ////worker.WorkPerformed += delegate (object sender, WorkPerformedEventArgs e)
+            ////{
+            ////    Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+            ////};
+
+            //mod to use Lambda
+            worker.WorkPerformed += (s,e) => Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+
+//            worker.WorkCompleted += Worker_WorkCompleted;
+
+            worker.WorkCompleted += (s,e) =>
+            {
+                Console.WriteLine("Worker is done");
+                Console.WriteLine("Send Him Home!");
+            };
+
+            //worker.WorkCompleted -= Worker_WorkCompleted;
+            worker.DoWork(8, WorkType.GenerateReports);
 
             Console.Read();
         }
 
-        static void DoWork(WorkPerformedHandler del)
-        {
-            del(5, WorkType.GoToMeetings);
-        }
+        //static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        //{
+        //    Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+        //}
 
-        static void WorkPerformed1(int hours,WorkType workType)
-        {
-            Console.WriteLine("WorkPerformed1 called " + hours.ToString());
-        }
+        ////static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        ////{
+        ////    //    throw new NotImplementedException();
+        ////    Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+        ////}
 
-        static void WorkPerformed2(int hours, WorkType workType)
-        {
-            Console.WriteLine("WorkPerformed2 called " + hours.ToString());
-        }
+        //static void Worker_WorkCompleted(object sender, EventArgs e)
+        //{
+        //    Console.WriteLine("Worker is done");
+        //}
+
+        //static void DoWork(WorkPerformedHandler del)
+        //{
+        //    del(5, WorkType.GoToMeetings);
+        //}
+
+        //static int WorkPerformed1(int hours,WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed1 called " + hours.ToString());
+        //    return hours + 1;
+        //}
+
+        //static int WorkPerformed2(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed2 called " + hours.ToString());
+        //    return hours + 2;
+        //}
+
+        //static int WorkPerformed3(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed3 called " + hours.ToString());
+        //    return hours + 3;
+
+        //}
 
     }
     public enum WorkType
--- Worker.cs
+++ Worker.cs
@@ -6,9 +6,60 @@ using System.Threading.Tasks;
 
 namespace PS_DelegatesAndEvents
 {
-    //public delegate int WorkPerformedHandler(int hours, WorkType workType);
+//    public delegate int WorkPerformedHandler(int hours, WorkType workType);
+//    public delegate int WorkPerformedHandler(object sender,WorkPerformedEventArgs e);
+
     public class worker
     {
+        //        public event WorkPerformedHandler WorkPerformed;
+        public event EventHandler<WorkPerformedEventArgs> WorkPerformed;
+        public event EventHandler WorkCompleted;
+
+        public void DoWork(int hours, WorkType workType)
+        {
+            for (int i = 0;i < hours; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                OnWorkPerformed(i + 1, workType);//raise event performed
+            }
+            OnWorkCompleted();//Raise event completed
+        }
+
+        protected virtual void OnWorkPerformed(int hours,WorkType workType)
+        {
+            //if (WorkPerformed != null)
+            //{
+            //    WorkPerformed(hours, workType);
+            //}
+
+            //preferred method
+//            var del = WorkPerformed as WorkPerformedHandler;
+            var del = WorkPerformed as EventHandler<WorkPerformedEventArgs>;
+            if (del != null)
+            {
+                del(this, new WorkPerformedEventArgs(hours, workType));
+            }
+       
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            //if (WorkCompleted != null)
+            //{
+            //    WorkCompleted(hours, workType);
+            //}
+
+            //preferred method
+            var del = WorkCompleted as EventHandler;
+            if (del != null)
+            {
+                del(this, EventArgs.Empty);
+            }
+
+        }
+
+
+
         //public event
 
     }
